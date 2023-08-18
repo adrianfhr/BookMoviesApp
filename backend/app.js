1006749f4107d4ca0b8b6de16d4cc52c8ff21c98
adrianfhr <adrianfahriaffandi@gmail.com>
2023-08-19 01:47:04 +0700

    deployment

---
+++ backend/app.js
@@ -0,0 +1,40 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user-routes.js";
+import adminRouter from "./routes/admin-routes.js";
+import movieRouter from "./routes/movie-routes.js";
+import bookingsRouter from "./routes/booking-routes.js";
+import cors from "cors";
+
+dotenv.config();
+const app = express();
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+app.use("/user", userRouter);
+app.use("/admin", adminRouter);
+app.use("/movie", movieRouter);
+app.use("/booking", bookingsRouter);
+
+(async () => {
+  try {
+    console.log("Starting MongoDB connection...");
+    await mongoose.connect(
+      `mongodb+srv://18221002:${process.env.MONGODB_PASSWORD}@cluster0.3vhi5xq.mongodb.net/`,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      }
+    );
+    console.log("Connected to MongoDB");
+
+    app.listen(5000, () => {
+      console.log("Server is running on port 5000");
+    });
+  } catch (error) {
+    console.error("MongoDB Connection Error:", error);
+  }
+})();
---
+++ backend/models/Movie.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+
+
+const movieSchema = new mongoose.Schema({
+  idFilm:{
+    type: Number,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  release_date: {
+    type: Date,
+    required: true,
+  },
+  poster_url: {
+    type: String,
+    required: true,
+  },
+  age_rating: {
+    type: Number,
+    required: true,
+  },
+  ticket_price: {
+    type: Number,
+    required: true,
+  },
+  featured: {
+    type: Boolean,
+    default: true,
+  },
+  bookings: [{ type: mongoose.Types.ObjectId, ref: "Booking" }],
+  admin: {
+    type: mongoose.Types.ObjectId,
+    ref: "Admin",
+    required: true,
+  },
+});
+
+export default mongoose.model("Movie", movieSchema);
